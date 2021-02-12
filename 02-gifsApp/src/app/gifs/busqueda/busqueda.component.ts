cb9a4f5403c44c205925d0d0b6f9bb2e0a936fb9
Starkin28 <victoreq00@gmail.com>
2021-02-12 11:01:17 +0100

    create historial with gifs service

--- 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
+++ 02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  constructor(private gifsService: GifsService) {}
 
-  ngOnInit(): void {
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+
+    this.gifsService.buscarGifs(valor);
+
+    this.txtBuscar.nativeElement.value = '';
   }
-
 }
--- 02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
+++ 02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  get historial() {
+    return this.gifsService.historial;
+  }
+
+  constructor(private gifsService: GifsService) {}
+}
