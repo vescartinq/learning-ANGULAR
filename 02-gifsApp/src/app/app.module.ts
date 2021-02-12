0ea5787683217a30834aef14f1412fa6ca18ea70
Starkin28 <victoreq00@gmail.com>
2021-02-12 10:11:53 +0100

    structuring html template incomponents and shared modules

--- 02-gifsApp/src/app/app.module.ts
+++ 02-gifsApp/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule, GifsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
