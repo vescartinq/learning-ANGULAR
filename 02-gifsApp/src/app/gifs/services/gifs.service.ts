ea76a29a0051d9da30d12f03507c49d69b7a6eb3
Starkin28 <victoreq00@gmail.com>
2021-02-15 11:13:39 +0100

    refactoring of http params request, and improving css

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
@@ -7,6 +7,7 @@ import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 })
 export class GifsService {
   private apiKey: string = 'KPeT2IO6lmQktxaxLNmLN1C0eG1TrUAR';
+  private servicioUrl = 'http://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
 
   public resultados: Gif[] = [];
@@ -15,7 +16,10 @@ export class GifsService {
     return [...this._historial];
   }
 
-  constructor(private http: HttpClient) {} //Observable, misma acción que promesas(axios)
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || []; //Muestra directamente la info de localStorage
+    this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+  } //Observable, misma acción que promesas(axios); solo se ejecuta la primera vez que es llamado
 
   buscarGifs(query: string = '') {
     query = query.trim().toLowerCase(); //valor en minuscula y sin espacios para evitar errores
@@ -24,15 +28,21 @@ export class GifsService {
       //si no existe el valor añadido
       this._historial.unshift(query); //insertar el último al principio
       this._historial = this._historial.splice(0, 10); //mostrar 10 resultados
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
+
     this.http
-      .get<SearchGifsResponse>(
-        `http://api.giphy.com/v1/gifs/search?api_key=KPeT2IO6lmQktxaxLNmLN1C0eG1TrUAR&q=${query}&limit=10`
-      )
+      .get<SearchGifsResponse>(`${this.servicioUrl}/search`, { params }) //consulta a la API
       .subscribe((resp) => {
         console.log(resp.data);
         this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
       });
   }
 }
--- 02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
+++ 02-gifsApp/src/app/shared/sidebar/sidebar.component.ts
@@ -11,4 +11,8 @@ export class SidebarComponent {
   }
 
   constructor(private gifsService: GifsService) {}
+
+  buscar(termino: string) {
+    this.gifsService.buscarGifs(termino);
+  }
 }
