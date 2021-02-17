133d7214ccf7728d67df99e897586b700015fcf2
Starkin28 <victoreq00@gmail.com>
2021-02-17 10:57:44 +0100

    managing errors

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -8,14 +8,21 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent {
   termino: string = '';
+  hayError: boolean = false;
 
   constructor(private paisService: PaisService) {}
 
   buscar() {
+    this.hayError = false;
     console.log(this.termino);
 
-    this.paisService.buscarPais(this.termino).subscribe((resp) => {
-      console.log(resp);
-    });
+    this.paisService.buscarPais(this.termino).subscribe(
+      (resp) => {
+        console.log(resp);
+      },
+      (error) => {
+        this.hayError = true;
+      }
+    );
   }
 }
