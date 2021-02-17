f10a37ce209d7815a9c1d4cc837eb15ccb99a7ec
Starkin28 <victoreq00@gmail.com>
2021-02-17 12:27:11 +0100

    refactoring input and table components

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -9,19 +10,22 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private paisService: PaisService) {}
 
-  buscar() {
+  buscar(termino: string) {
     this.hayError = false;
-    console.log(this.termino);
+    this.termino = termino;
 
-    this.paisService.buscarPais(this.termino).subscribe(
+    this.paisService.buscarPais(termino).subscribe(
       (paises) => {
         console.log(paises);
+        this.paises = paises;
       },
       (error) => {
         this.hayError = true;
+        this.paises = [];
       }
     );
   }
