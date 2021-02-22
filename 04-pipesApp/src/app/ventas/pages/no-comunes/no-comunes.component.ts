6b43fd1a08bc1806cac700f979f6a83e6e3e8430
Starkin28 <victoreq00@gmail.com>
2021-02-22 13:56:44 +0100

    i18select i18plural to manage sex or plural args

--- 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,16 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-comunes',
   templateUrl: './no-comunes.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class NoComunesComponent implements OnInit {
+export class NoComunesComponent {
+  // i18nSelect
+  nombre1: string = 'Victor';
+  genero1: string = 'masculino';
+  nombre2: string = 'Noa';
+  genero2: string = 'femenino';
 
-  constructor() { }
+  invitacionMap = {
+    masculino: 'invitarlo',
+    femenino: 'invitarla',
+  };
 
-  ngOnInit(): void {
-  }
+  // i18Plural
+  clientes0: string[] = [];
+  clientes1: string[] = ['a'];
+  clientes2: string[] = ['a', 'b'];
+  clientes5: string[] = ['a', 'b', 'c', 'd', 'e'];
 
+  clientesMap = {
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos dos clientes esperando',
+    other: 'tenemos # clientes esperando',
+  };
 }
