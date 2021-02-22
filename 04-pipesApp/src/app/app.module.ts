809832df21ad3f80f33fc0916203586d0ce89598
Starkin28 <victoreq00@gmail.com>
2021-02-22 10:35:06 +0100

    menubar done

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 // Modulo personalizado
-import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PrimeNgModule],
+  imports: [BrowserModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- 04-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 04-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 
-// // PrimeNg
+// PrimeNg
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
-  exports: [ButtonModule, CardModule],
+  exports: [ButtonModule, CardModule, MenubarModule],
 })
 export class PrimeNgModule {}
