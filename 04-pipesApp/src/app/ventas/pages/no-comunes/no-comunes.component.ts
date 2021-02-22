51f8f97f352c770055ecce0f90cad6822022cf49
Starkin28 <victoreq00@gmail.com>
2021-02-22 18:16:15 +0100

    slicePipe, KeyValuePipe, JsonPipe and AsyncPipe

--- 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -20,13 +21,52 @@ export class NoComunesComponent {
   // i18Plural
   clientes0: string[] = [];
   clientes1: string[] = ['a'];
-  clientes2: string[] = ['a', 'b'];
   clientes5: string[] = ['a', 'b', 'c', 'd', 'e'];
 
   clientesMap = {
     '=0': 'no tenemos ningún cliente esperando',
     '=1': 'tenemos un cliente esperando',
-    '=2': 'tenemos dos clientes esperando',
     other: 'tenemos # clientes esperando',
   };
+
+  cambiarCliente() {
+    this.nombre1 = 'Esther';
+    this.genero1 = 'femenino';
+  }
+
+  borrarCliente() {
+    this.clientes5.pop();
+  }
+
+  // KeyValue Pipe
+  persona = {
+    nombre: 'Victor',
+    edad: 39,
+    direccion: 'Barcelona',
+  };
+
+  // Json Pipe
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+    },
+    {
+      nombre: 'Wonder Woman',
+      vuela: false,
+    },
+  ];
+
+  // Async Pipe
+  miObservable = interval(2000); // 0,1,2,3,4,5,6
+
+  valorPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de promesa pasados 3,5 seg');
+    }, 3500);
+  });
 }
