2032f114d4e0d9e8d65c94afa59f573a37ef6f9e
Starkin28 <victoreq00@gmail.com>
2021-02-22 11:00:07 +0100

    created labels on menubar

--- 04-pipesApp/src/app/shared/menu/menu.component.ts
+++ 04-pipesApp/src/app/shared/menu/menu.component.ts
@@ -14,24 +14,29 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void {
     this.items = [
       {
-        label: 'File',
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
         items: [
           {
-            label: 'New',
-            icon: 'pi pi-fw pi-plus',
-            items: [{ label: 'Project' }, { label: 'Other' }],
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+            routerLink: '/',
           },
-          { label: 'Open' },
-          { label: 'Quit' },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+            routerLink: 'numeros',
+          },
+          {
+            label: 'No Comunes',
+            icon: 'pi pi-globe',
+            routerLink: 'no-comunes',
+          },
         ],
       },
       {
-        label: 'Edit',
-        icon: 'pi pi-fw pi-pencil',
-        items: [
-          { label: 'Delete', icon: 'pi pi-fw pi-trash' },
-          { label: 'Refresh', icon: 'pi pi-fw pi-refresh' },
-        ],
+        label: 'Pipes Personalizados',
+        icon: 'pi pi-cog',
       },
     ];
   }
