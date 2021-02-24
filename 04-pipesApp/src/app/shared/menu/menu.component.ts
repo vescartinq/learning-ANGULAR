27cb916e2539d304b7a5fd6a2d8e30175c6694d5
Starkin28 <victoreq00@gmail.com>
2021-02-24 11:05:28 +0100

    handmade table vs primeNg table

--- 04-pipesApp/src/app/shared/menu/menu.component.ts
+++ 04-pipesApp/src/app/shared/menu/menu.component.ts
@@ -37,6 +37,7 @@ export class MenuComponent implements OnInit {
       {
         label: 'Pipes Personalizados',
         icon: 'pi pi-cog',
+        routerLink: 'ordenar',
       },
     ];
   }
---
+++ 04-pipesApp/src/app/ventas/pipes/vuela.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'vuela',
+})
+export class VuelaPipe implements PipeTransform {
+  transform(valor: boolean): string {
+    return valor ? 'vuela' : 'no vuela';
+  }
+}
