72d15553c6f71669cbdd8dc1668e57c8f6e20651
Starkin28 <victoreq00@gmail.com>
2021-02-25 14:46:25 +0100

    autocomplete when not found any result working well

--- 05-heroesApp/src/app/heroes/pages/buscar/buscar.component.ts
+++ 05-heroesApp/src/app/heroes/pages/buscar/buscar.component.ts
@@ -12,7 +12,7 @@ import { HeroesService } from '../../services/heroes.service';
 export class BuscarComponent implements OnInit {
   termino: string = '';
   heroes: Heroe[] = [];
-  heroeSeleccionado!: Heroe;
+  heroeSeleccionado: Heroe | undefined;
 
   constructor(private heroesService: HeroesService) {}
 
@@ -20,11 +20,16 @@ export class BuscarComponent implements OnInit {
 
   buscando() {
     this.heroesService
-      .getSugerencias(this.termino)
+      .getSugerencias(this.termino.trim())
       .subscribe((heroes) => (this.heroes = heroes));
   }
 
   opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+    if (!event.option.value) {
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
     const heroe: Heroe = event.option.value;
     this.termino = heroe.superhero;
 
