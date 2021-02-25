44144adf97eff6d940af18c756a02b432abafe8f
Starkin28 <victoreq00@gmail.com>
2021-02-25 11:43:48 +0100

    drawing heroe cards on list page

--- 05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
+++ 05-heroesApp/src/app/heroes/pages/listado/listado.component.ts
@@ -5,7 +5,13 @@ import { Heroe } from '../../interfaces/heroes.interface';
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [],
+  styles: [
+    `
+      mat-card {
+        margin-top: 20px;
+      }
+    `,
+  ],
 })
 export class ListadoComponent implements OnInit {
   constructor(private heroesService: HeroesService) {}
