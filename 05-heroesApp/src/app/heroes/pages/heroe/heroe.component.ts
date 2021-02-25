4625b4df0aab4311b87fc6aefb81cb38bc0cd862
Starkin28 <victoreq00@gmail.com>
2021-02-25 12:47:08 +0100

    getting heroe id by params

--- 05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
+++ 05-heroesApp/src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroes.interface';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class HeroeComponent implements OnInit {
-
-  constructor() { }
+  // activated Route para obtener params
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    // obtener id del personaje
+    this.activatedRoute.params.subscribe(({ id }) => console.log(id));
   }
-
 }
