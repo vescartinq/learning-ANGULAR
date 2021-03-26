4529cfee21f31ea9ec8887324812021948d6a7ff
Starkin28 <victoreq00@gmail.com>
2021-03-26 05:56:52 +0100

    lifeCycleHooks

---
+++ 08-lifeCycleHooks/src/app/components/muestra-nombre/muestra-nombre.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-muestra-nombre',
+  templateUrl: './muestra-nombre.component.html',
+  styles: [
+  ]
+})
+export class MuestraNombreComponent implements OnInit, OnChanges {
+
+  @Input() nombre!: string;
+
+  constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
